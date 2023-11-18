8c8d141bc82aa6b169e382afd0e2e8a9221db01e
Jesus <janeirojm91@gmail.com>
2023-11-17 21:43:41 -0600

    creacion de components

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,15 @@
 import React from "react";
 import "./App.css";
 import Card from "./Components/moviecards/Card.jsx";
+import Landing from "./Components/landing/Landing.jsx";
+import MovieDescription from "./Components/moviedescription/MovieDescription.jsx"
 
 export default function App() {
   return (
     <>
-      <div className="row">
-        <div className="col" >
-          <Card />
-        </div>
-        <div className="col">
-          <Card />
-        </div>
-        <div className="col">
-          <Card />
-        </div>
-        <div className="col">
-          <Card />
-        </div>
-      </div>
+     {/*  <Landing /> */}
+     {/*  <Card /> */}
+     {/* <MovieDescription /> */}
     </>
   );
 }
---
+++ src/Components/landing/Landing.jsx
@@ -0,0 +1,17 @@
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
+export default function Landing() {
+  return (
+    <Card style={{ width: '18rem' }}>
+      
+      <Card.Body>
+        <Card.Title id='card-title'>Card Title</Card.Title>
+        <Card.Img variant="top" src="" />
+        <Card.Title id='card-title'>Card Title</Card.Title>
+        <Card.Img variant="top" src="" />
+        
+      </Card.Body>
+    </Card>
+  );
+  }
